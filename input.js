98d39108eb20e8d498699676b99393ea308d897b
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-06-08 19:52:12 -0600

    input module 2022-0608

---
+++ input.js
@@ -0,0 +1,30 @@
+//should check for the ctrl + c input and terminate the game.
+const handleUserInput = function (key) {
+  // on any input from stdin (standard input), output a "." to stdout
+  //process.stdout.write('.');
+  
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+
+}
+
+//setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  
+  // Event Handling for User Input
+  stdin.on("data", handleUserInput);
+  
+  return stdin;
+};
+
+setupInput();
+
+module.exports = {
+  setupInput,
+};
