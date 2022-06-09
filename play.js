1a9db55a1d89a34fcb935149286352b4fbfce443
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-06-08 19:53:13 -0600

    setupInput  2022-0608

--- play.js
+++ play.js
@@ -1,31 +1,7 @@
 const { connect } = require("./client");
-// const { setupInput } = require("./input");
+const { setupInput } = require("./input");
 
-
-//should check for the ctrl + c input and terminate the game.
-const handleUserInput = function (key) {
-  // on any input from stdin (standard input), output a "." to stdout
-  //process.stdout.write('.');
-  
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
-  }
-
-}
-
-
-//setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  
-  // Event Handling for User Input
-  stdin.on("data", handleUserInput);
-  
-  return stdin;
-};
+console.log("Connecting ...");
+connect();
 
 setupInput();
