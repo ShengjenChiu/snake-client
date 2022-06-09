bb68b7365012cfbd84c44fd20d89876ee712bf21
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-06-08 18:29:08 -0600

    play.js-3 2022-0608

--- play.js
+++ play.js
@@ -1,25 +1,7 @@
-// const { connect } = require("./client");
+const { connect } = require("./client");
 // const { setupInput } = require("./input");
 
-const net = require("net");
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  });
-  
-  conn.setEncoding("utf8");
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
 
 //setupInput();
 
