6dbf3c2a33cc7f77a89db1eeba581db4066ddd0b
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-06-08 18:28:23 -0600

    the client  2022-0608

---
+++ client.js
@@ -0,0 +1,28 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541
+  });
+  
+  conn.setEncoding("utf8");
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: SJC");
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = {
+  connect,
+};
