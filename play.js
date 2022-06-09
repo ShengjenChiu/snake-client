b78e130e4fef5d3041266cdcda1abb13b1da06e8
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-06-08 19:44:22 -0600

    stdin-out 2022-0608

--- play.js
+++ play.js
@@ -2,33 +2,30 @@ const { connect } = require("./client");
 // const { setupInput } = require("./input");
 
 
-
-//setupInput();
-
-
-// // setup interface to handle user input from stdin
-
-// const setupInput = function () {
-//   const stdin = process.stdin;
-//   stdin.setRawMode(true);
-//   stdin.setEncoding("utf8");
-//   stdin.resume();
+//should check for the ctrl + c input and terminate the game.
+const handleUserInput = function (key) {
+  // on any input from stdin (standard input), output a "." to stdout
+  //process.stdout.write('.');
   
-//   // Event Handling for User Input
-//   stdin.on("data", handleUserInput);
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+
+}
+
+
+//setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
   
-//   return stdin;
-// };
-
-
-// //should check for the ctrl + c input and terminate the game.
-// const handleUserInput = function (key) {
-//   // on any input from stdin (standard input), output a "." to stdout
-//   process.stdout.write('.');
+  // Event Handling for User Input
+  stdin.on("data", handleUserInput);
   
-//   // \u0003 maps to ctrl+c input
-//   if (key === '\u0003') {
-//     process.exit();
-//   }
+  return stdin;
+};
 
-// }
+setupInput();
