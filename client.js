947232a301f70b30c825161a3dd965191151f890
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-06-08 19:30:14 -0600

    moves 2022-0608

--- client.js
+++ client.js
@@ -15,6 +15,74 @@ const connect = function() {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: SJC");
+
+
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 100);
+
+    // setInterval(() => {
+    //   conn.write("Move: right");
+    // }, 100);
+
+    // setInterval(() => {
+    //   conn.write("Move: down");
+    // }, 100);
+    
+    // setInterval(() => {
+    //   conn.write("Move: left");
+    // }, 100);
+
+
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 0);
+
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 0);
+
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 50);
+
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 100);
+
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 150);
+
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 200);
+
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 250);
+
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 300);
+
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 350);
+
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 400);
+
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 450);
+
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 500);
+
+
   });
 
   return conn;
